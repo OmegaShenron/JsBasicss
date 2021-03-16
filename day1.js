24723068e4ca0293fae05a57d533d7a40239209e
AlejandroTCX <alex.6161@hotmail.com>
2021-03-16 11:41:21 -0600

    Pruebas

---
+++ day1.js
@@ -0,0 +1,23 @@
+//comments can make code readable
+//Bienvenido a 30 Días de JavaScript
+/* comments can make code readable
+    easy to reuse and informative */
+let variable1="Empezando con un carácter";
+let variable2=true;
+let variable3;
+let variable4=null;
+console.log(typeof variable1);
+console.log(typeof variable2);
+console.log(typeof variable3);
+console.log(typeof variable4);
+let variable11=10, variable12=9, variable13=8, variable14=11;
+var variable15, variable16, variable17, variable18;
+
+var nombre=prompt("");
+var apellido=prompt("");
+var edad=prompt("");
+var estadoCivil=prompt("");
+var pais=prompt("");
+var myAge=20, yourAge=27;
+console.log("mi edad es:"+myAge);
+console.log("tu edad es: "+yourAge);
